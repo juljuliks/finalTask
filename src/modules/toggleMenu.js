e25d458b0cd82651a630cf0bfb97115d901f64de
Julia Glukhova <juliaglukhova666@gmail.com>
2021-05-13 14:30:32 +0300

    add toggleMenu mobile version

--- src/modules/toggleMenu.js
+++ src/modules/toggleMenu.js
@@ -1,10 +1,16 @@
 const toggleMenu = () => {
+    console.log(screen.width);
     const menu = document.querySelector('.popup-dialog-menu');
     window.addEventListener('click', (e) => {
+
         if (e.target.matches('.menu__icon')) {
             menu.style.transform = 'translate3d(0px, 0px, 0px)';
         } else if (e.target.matches('.close-menu') || e.target.matches('.menu-link') || e.target.closest('.main')) {
-            menu.style.transform = 'translate3d(645px, 0px, 0px)';
+            if (screen.width < 576) {
+                menu.style.transform = 'translate3d(0px, -100vh, 0px)';
+            } else {
+                menu.style.transform = 'translate3d(645px, 0px, 0px)';
+            }
         }
     })
 };
