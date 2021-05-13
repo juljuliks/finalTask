0b74f31f979710ca4d3e8938c91eaf6e5da485bb
Julia Glukhova <juliaglukhova666@gmail.com>
2021-05-13 19:19:52 +0300

    add toggle consult popup

--- src/modules/dist/toggleConsultPopup.dev.js
+++ src/modules/dist/toggleConsultPopup.dev.js
@@ -7,9 +7,9 @@ exports["default"] = void 0;
 
 var toggleConsultPopup = function toggleConsultPopup() {
   window.addEventListener('click', function (e) {
-    if (e.target.matches('.button, .button_wide, .mobile-hide, .tablet-hide')) {
+    if (e.target.matches('.button_wide, .mobile-hide, .tablet-hide')) {
       document.querySelector('.popup-consultation').style.visibility = 'visible';
-    } else if (!e.target.closest('.feedback-wrap')) {
+    } else if (!e.target.closest('.feedback-wrap') || e.target.matches('.close-consultation')) {
       document.querySelector('.popup-consultation').style.visibility = 'hidden';
     }
   });
--- src/modules/toggleConsultPopup.js
+++ src/modules/toggleConsultPopup.js
@@ -1,8 +1,8 @@
 const toggleConsultPopup = () => {
     window.addEventListener('click', (e) => {
-        if (e.target.matches('.button, .button_wide, .mobile-hide, .tablet-hide')) {
+        if (e.target.matches('.button_wide, .mobile-hide, .tablet-hide')) {
             document.querySelector('.popup-consultation').style.visibility = 'visible';
-        } else if (!e.target.closest('.feedback-wrap')) {
+        } else if (!e.target.closest('.feedback-wrap') || e.target.matches('.close-consultation')) {
             document.querySelector('.popup-consultation').style.visibility = 'hidden';
         }
     })
