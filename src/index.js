5978139ac0fa61d0517c21103e2394d8563e0fa7
Julia Glukhova <juliaglukhova666@gmail.com>
2021-05-13 16:36:03 +0300

    add mask for phone inputs

--- src/index.js
+++ src/index.js
@@ -5,9 +5,11 @@ import toggleNumber from './modules/toggleNumber';
 import scroll from './modules/scroll';
 import togglePricePopup from './modules/togglePricePopup';
 import togglePrivacyPopup from './modules/togglePrivacyPopup';
+import maskPhone from './modules/maskPhone';
 
 toggleMenu();
 toggleNumber();
 scroll();
 togglePricePopup();
-togglePrivacyPopup()
+togglePrivacyPopup();
+maskPhone();
---
+++ src/modules/maskPhone.js
@@ -0,0 +1,72 @@
+const maskPhone = () => {
+    let phoneInputs = document.querySelectorAll('input[name=phone]');
+
+    let getInputNumbersValue = function (input) {
+        return input.value.replace(/\D/g, '');
+    }
+
+    const onPhonePaste = function (e) {
+        let input = e.target,
+            inputNumbersValue = getInputNumbersValue(input);
+        let pasted = e.clipboardData || window.clipboardData;
+        if (pasted) {
+            let pastedText = pasted.getData('Text');
+            if (/\D/g.test(pastedText)) {
+                input.value = inputNumbersValue;
+                return;
+            }
+        }
+    }
+
+    const onPhoneInput = function (e) {
+        let input = e.target,
+            inputNumbersValue = getInputNumbersValue(input),
+            selectionStart = input.selectionStart,
+            formattedInputValue = "";
+
+        if (!inputNumbersValue) {
+            return input.value = "";
+        }
+
+        if (input.value.length != selectionStart) {
+            if (e.data && /\D/g.test(e.data)) {
+                input.value = inputNumbersValue;
+            }
+            return;
+        }
+
+        if (["7", "8", "9"].indexOf(inputNumbersValue[0]) > -1) {
+            if (inputNumbersValue[0] == "9") inputNumbersValue = "7" + inputNumbersValue;
+            let firstSymbols = (inputNumbersValue[0] == "8") ? "8" : "+7";
+            formattedInputValue = input.value = firstSymbols + " ";
+            if (inputNumbersValue.length > 1) {
+                formattedInputValue += '(' + inputNumbersValue.substring(1, 4);
+            }
+            if (inputNumbersValue.length >= 5) {
+                formattedInputValue += ') ' + inputNumbersValue.substring(4, 7);
+            }
+            if (inputNumbersValue.length >= 8) {
+                formattedInputValue += '-' + inputNumbersValue.substring(7, 9);
+            }
+            if (inputNumbersValue.length >= 10) {
+                formattedInputValue += '-' + inputNumbersValue.substring(9, 11);
+            }
+        }
+        input.value = formattedInputValue;
+    }
+
+    const onPhoneKeyDown = function (e) {
+        let inputValue = e.target.value.replace(/\D/g, '');
+        if (e.keyCode == 8 && inputValue.length == 1) {
+            e.target.value = "";
+        }
+    }
+    
+    for (let phoneInput of phoneInputs) {
+        phoneInput.addEventListener('keydown', onPhoneKeyDown);
+        phoneInput.addEventListener('input', onPhoneInput);
+        phoneInput.addEventListener('paste', onPhonePaste);
+    }
+}
+
+export default maskPhone;
