4a6aaaded1e9be0adf2990a17219e8fcdbaadaa9
Julia Glukhova <juliaglukhova666@gmail.com>
2021-05-13 19:33:25 +0300

    add toggle consult popup

--- src/modules/toggleConsultPopup.js
+++ src/modules/toggleConsultPopup.js
@@ -1,8 +1,9 @@
 const toggleConsultPopup = () => {
     window.addEventListener('click', (e) => {
-        if (e.target.matches('.button_wide, .mobile-hide, .tablet-hide')) {
+        if (e.target.matches('.button_wide, .mobile-hide, .tablet-hide') && e.target.closest('.director-avatar')) {
             document.querySelector('.popup-consultation').style.visibility = 'visible';
-        } else if (!e.target.closest('.feedback-wrap') || e.target.matches('.close-consultation')) {
+        }
+        else if (!e.target.closest('.feedback-wrap') || e.target.matches('.close-consultation')) {
             document.querySelector('.popup-consultation').style.visibility = 'hidden';
         }
     })
