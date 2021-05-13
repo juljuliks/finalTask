0224cbb5aa925de432bda2b36feeb19de2d29caf
Julia Glukhova <juliaglukhova666@gmail.com>
2021-05-13 15:56:40 +0300

    add toggle privacy popup

--- src/index.js
+++ src/index.js
@@ -4,9 +4,10 @@ import toggleMenu from './modules/toggleMenu';
 import toggleNumber from './modules/toggleNumber';
 import scroll from './modules/scroll';
 import togglePricePopup from './modules/togglePricePopup';
-
+import togglePrivacyPopup from './modules/togglePrivacyPopup';
 
 toggleMenu();
 toggleNumber();
 scroll();
 togglePricePopup();
+togglePrivacyPopup()
---
+++ src/modules/togglePrivacyPopup.js
@@ -0,0 +1,13 @@
+const togglePrivacyPopup = () => {
+    const main = document.getElementById('main'),
+          privacyPopup = document.querySelector('.popup-privacy');
+    window.addEventListener('click', (e) => {
+        if (e.target.matches('.link-privacy')) {
+            privacyPopup.style.visibility = 'visible';
+        } else if (!e.target.closest('.popup-dialog-privacy')) {
+            privacyPopup.style.visibility = 'hidden';
+        }
+    })
+}
+
+export default togglePrivacyPopup;
