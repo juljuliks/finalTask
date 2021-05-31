1140524a1d438a1f06b37e9abc814bc825f42da4
Julia Glukhova <juliaglukhova666@gmail.com>
2021-05-31 16:54:51 +0300

    fix

--- src/modules/faqAccordion.js
+++ src/modules/faqAccordion.js
@@ -1,29 +1,29 @@
 const faqAccordion = () => {
-
-  const accordeonItems = document.querySelectorAll('div.accordion>ul>li')
+  const accordeonItems = document.querySelectorAll("div.accordion>ul>li");
 
   const classRemover = () => {
-    accordeonItems.forEach(el => {
-        if (el.children[0].classList.contains('msg-active')) {
-            el.children[0].classList.remove('msg-active')
-        }
-    })
-  }
-  
+    accordeonItems.forEach((el) => {
+      if (el.children[0].classList.contains("msg-active")) {
+        el.children[0].classList.remove("msg-active");
+      }
+    });
+  };
+
   const toggleAccordion = (e) => {
-    if (e.target.closest('.title_block')) {
-      if (e.target.classList.contains('msg-active')) {
-        e.target.classList.remove('msg-active');
+    if (e.target.closest(".title_block")) {
+      if (e.target.classList.contains("msg-active")) {
+        e.target.classList.remove("msg-active");
       } else {
-        classRemover()
-        e.target.classList.add('msg-active');
+        classRemover();
+        e.target.classList.add("msg-active");
       }
     }
-  }
+  };
 
-  accordeonItems.forEach(el => {
-    el.addEventListener('click', toggleAccordion)
-  })
-}
+  accordeonItems.forEach((el) => {
+    el.addEventListener("click", toggleAccordion);
+  });
+
+};
 
 export default faqAccordion;
